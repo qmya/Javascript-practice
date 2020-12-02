2d304cd4f36193e3d9b7d8d0032f49469c676b87
qmya <qandeelgul90@gmail.com>
2020-12-02 11:12:40 -0500

    practicing javascript from udemy course day 1

---
+++ script.js
@@ -0,0 +1,47 @@
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is fun");
+console.log(40 + 100 + 30);
+
+let firstName = "Qandeel";
+console.log(firstName);
+let age = 25;
+console.log(age);
+
+//Data types
+console.log(typeof age);
+
+let JavascriptIsFun = true;
+console.log(JavascriptIsFun);
+JavascriptIsFun = "Yes!";
+console.log(JavascriptIsFun);
+
+let year;
+console.log(year);
+
+year = 1995;
+console.log(year);
+// age in 2050
+
+const now = 2020;
+const agePurdal = now - 1986;
+const ageQandeel = now - 1995;
+console.log(ageQandeel, agePurdal);
+//2**3 means 2 to the power of 3
+console.log(ageQandeel * 2, agePurdal / 2, 2 ** 3);
+
+//+: is used to add/join or concatinate two strings
+const lastName = "Mya";
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//assignment operators
+
+let x = 10 + 5; //x=15
+x += 10; //we add 10 to the 15 = 25
+x++; //x=x+1
+console.log(x);
+
+//Comparison operators
+//Results of a comparison operators should be a booleans
+console.log(agePurdal > ageQandeel); //true
+console.log(ageQandeel >= 26);
